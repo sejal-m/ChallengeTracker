354affc1041bc3326267cdfdfec6803cd2ec13be
sejal-m <sejalmaurya27@gmail.com>
2021-08-31 20:45:07 +0530

    set up basic gridview displaying days 1-100

---
+++ app/src/main/java/com/example/challegetracker/GridAdapter.java
@@ -0,0 +1,40 @@
+package com.example.challegetracker;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class GridAdapter extends BaseAdapter {
+    Context context;
+    int[] days;
+    LayoutInflater inflater;
+    public GridAdapter(Context applicationContext, int[] days) {
+        this.context = applicationContext;
+        this.days = days;
+        inflater = (LayoutInflater.from(applicationContext));
+    }
+    @Override
+    public int getCount() {
+        return days.length;
+    }
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = inflater.inflate(R.layout.activity_gridview, null); // inflate the layout
+        TextView number_view = view.findViewById(R.id.day_number); // get the reference of ImageView
+        number_view.setText(String.valueOf(days[i])); // set logo images
+        return view;
+
+        
+    }
+}
--- app/src/main/java/com/example/challegetracker/MainActivity.java
+++ app/src/main/java/com/example/challegetracker/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.challegetracker;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class MainActivity extends AppCompatActivity {
+    GridView challenge_grid;
+    TextView day_number;
+    int[] days = new int[100];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        populate(days); //populate array with 1-100
+        challenge_grid = findViewById(R.id.challengeContainer); // initialising the gridview
+
+        // Create an object of CustomAdapter and set Adapter to GirdView
+        GridAdapter gridAdapter = new GridAdapter(this, days);
+        challenge_grid.setAdapter(gridAdapter);
+
+    }
+
+    public void populate(int[] a) {
+        for (int i = 0; i < 100; i++) {
+            a[i] = i + 1;
+        }
     }
 }
